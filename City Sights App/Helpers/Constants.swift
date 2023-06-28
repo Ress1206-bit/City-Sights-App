95fb15d95caf9872e054d7de7fb284696ad10047
Ress1206-bit <adam.ress@icloud.com>
2023-06-28 19:48:51 -0400

    Set up Yelp API and Parsed it into JSON Data

---
+++ City Sights App/Helpers/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  City Sights App
+//
+//  Created by Adam Ress on 6/28/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var apiKey = "poQlG6kyJ4z0Bjs5JXN7V_uInUYPjYNtB5W56pTnXRCdju0Pvu9jEY7oj85brw1U0RdB8oXTOnz43_hxreqPVCrYJTrzmWDf1Z_Q-F5rhtMAeGD2_T0GDiEWrrmcZHYx"
+    static var apiUrl = "https://api.yelp.com/v3/businesses/search"
+    static var restaurantsKey = "restaurants"
+    static var sightsKey = "arts"
+}
